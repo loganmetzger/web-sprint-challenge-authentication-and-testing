feb4932c6c3ad417ae0d1fbc3c52431a9e570ca8
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-17 17:05:46 -0400

    finished

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -8,9 +9,14 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+server.use(morgan('dev'))
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({ api: 'you got it' })
+})
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
---
+++ auth/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtSecret: process.env.JWt_SECRET || 'shhhhh'
+  }
